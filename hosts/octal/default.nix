ddaf785e4608e64f79312a4b310a461d05f9a797
aoli-al <aoli.al@hotmail.com>
2025-08-24 13:31:08 -0400

    update

--- hosts/octal/default.nix
+++ hosts/octal/default.nix
@@ -10,4 +10,9 @@
 
   boot.kernel.sysctl."kernel.perf_event_paranoid" = 1;
   networking.hostName = "octal";
+  environment.etc."hypr/hyprland.conf".text = ''
+    monitor=DP-6, 3840x2160@240, 0x0, 1.5
+    
+    # Add any other Hyprland config here
+  '';
 }
