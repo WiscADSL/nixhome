e6ffd1c9381407f5750d39c564fa08251021e19b
aoli-al <aoli.al@hotmail.com>
2025-08-25 10:45:39 -0400

    add key remap.

--- hosts/ruby/default.nix
+++ hosts/ruby/default.nix
@@ -8,4 +8,22 @@
     ../common/aoli.nix
   ];
   networking.hostName = "ruby";
+
+  systemd.services.kanata-default.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
+  services.kanata = {
+    enable = true;
+    keyboards.default = {
+      config =  builtins.readFile ./logic.kbd;
+      devices = [
+        "/dev/input/by-id/usb-Topre_REALFORCE_87_US-event-kbd"
+      ];
+    };
+  };
+
 }
