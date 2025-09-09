b0a8715059a7d353e24d6c0ee6e9eaa6862c377c
aoli-al <aoli.al@hotmail.com>
2025-09-09 13:40:25 -0400

    enable gestures

--- programs/hyprland/default.nix
+++ programs/hyprland/default.nix
@@ -101,6 +101,16 @@
           natural_scroll = true;
         };
       };
+      gestures = {
+        workspace_swipe = true;
+        workspace_swipe_fingers = 3;
+        workspace_swipe_distance = 300;
+        workspace_swipe_invert = true;
+        workspace_swipe_min_speed_to_force = 30;
+        workspace_swipe_cancel_ratio = 0.5;
+        workspace_swipe_create_new = true;
+        workspace_swipe_forever = false;
+      };
       general = {
         gaps_in = 0;
         gaps_out = 0;
